72429839f06bb2f33f144b2921a718edff927cd3
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2023-05-18 23:27:30 -0400

    Create search and list views

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
+import { SafeAreaView, StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.root}>
+        <View style={styles.searchContainer}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto"/>
+    </>
   );
 }
 
+const isAndroid = Platform.OS === 'android';
+
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: isAndroid ? StatusBar.currentHeight : 0,
   },
+  searchContainer: {
+    backgroundColor: 'green',
+    padding: 10,
+  },
+  listContainer: {
+    backgroundColor: 'red',
+    flex: 1,
+    flexGrow: 1,
+    padding: 10
+  }
 });
