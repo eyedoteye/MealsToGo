822c75793dba5266703ffeb4c9c82bf2540639b1
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2023-05-22 13:00:06 -0400

    Add bottom tabs

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { View, Text } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastucture/theme";
@@ -9,6 +12,20 @@ import {
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+const SettingsScreen = () => (
+  <View>
+    <Text>Settings!</Text>
+  </View>
+);
+
+const MapScreen = () => (
+  <View>
+    <Text>Map!</Text>
+  </View>
+);
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -25,7 +42,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
